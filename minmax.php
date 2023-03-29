95ccc4b880f2b6a65a4a20ed2c08e5c6aa8be18f
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-03-29 13:11:37 +0900

    minmax array

--- minmax.php
+++ minmax.php
@@ -1,30 +1,44 @@
 <?php
+// $arr = array(5, 10, 7, 3, 1);
+// $min = $arr[0];
+// $max = $arr[0];
+
+// for ($i = 1; $i < count($arr); $i++) {
+//     if ($arr[$i] < $min) {
+//         $min = $arr[$i];
+//     }
+//     if ($arr[$i] > $max) {
+//         $max = $arr[$i];
+//     }
+// }
+
+// echo "최소값: " . $min . "\n";
+// echo "최대값: " . $max . "\n";
+
 $arr = array(5, 10, 7, 3, 1);
-$min = $arr[0];
-$max = $arr[0];
-
-for ($i = 1; $i < count($arr); $i++) {
-    if ($arr[$i] < $min) {
-        $min = $arr[$i];
-    }
-    if ($arr[$i] > $max) {
-        $max = $arr[$i];
-    }
-}
-
-echo "최소값: " . $min . "\n";
-echo "최대값: " . $max . "\n";
 
 function myMin($array)
 {
+    $minResult = $array[0];
     for ($i=1; $i < count($array); $i++) {
-        $min = $array[0];
-        if ($array[$i] < $min) {
-            $min = $array[$i];
+        if ($array[$i] < $minResult) {
+            $minResult = $array[$i];
         }
     }
-    echo "최소값: " . $min . "\n";
+    echo "최소값: " . $minResult . "\n";
+}
+
+function myMax($array)
+{
+    $maxResult = $array[0];
+    for ($i=1; $i < count($array); $i++) {
+        if ($array[$i] > $maxResult) {
+        $maxResult = $array[$i];
+        }
+    }
+    echo "최대값: " . $maxResult . "\n";
 }
 
 myMin($arr);
+myMax($arr);
 ?>
