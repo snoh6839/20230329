c83c0beed1e781fcc5564c15f6afa0e4cf1c8a08
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-03-29 16:51:12 +0900

    gg

--- arraysort.php
+++ arraysort.php
@@ -4,47 +4,47 @@
 //     echo $i.">".$arr[$i]."\n";
 // }
 
-//1
+// 1
 
-// $arr = array(5, 10, 7, 3, 1);
+$arr = array(5, 10, 7, 3, 1);
 
-// for ($i = 0; $i < count($arr); $i++) {
-//     for ($j = 0; $j < count($arr) - $i - 1; $j++) {
-//         if ($arr[$j] > $arr[$j+1]) {
-//             $temp = $arr[$j];
-//             $arr[$j] = $arr[$j+1];
-//             $arr[$j+1] = $temp;
-//         }
-//     }
-// }
+for ($i = 0; $i < count($arr); $i++) {
+    for ($j = 0; $j < count($arr) - $i - 1; $j++) {
+        if ($arr[$j] > $arr[$j+1]) {
+            $temp = $arr[$j];
+            $arr[$j] = $arr[$j+1];
+            $arr[$j+1] = $temp;
+        }
+    }
+}
 
-// for ($i=0; $i < count($arr); $i++) {
-//     echo $i . " > " . $arr[$i] . "\n";
-// }
-// // var_dump($arr);
+for ($i=0; $i < count($arr); $i++) {
+    echo $i . " > " . $arr[$i] . "\n";
+}
+// var_dump($arr);
 
-// //2
+//2
 
-// $arr = array(5, 10, 7, 3, 1);
-// $cntArr = count($arr);
+$arr = array(5, 10, 7, 3, 1);
+$cntArr = count($arr);
 
-// for ($i = 0; $i < $cntArr; $i++) {
-//     $minIndex = $i;
-//     for ($j = $i + 1; $j < $cntArr; $j++) {
-//         if ($arr[$j] < $arr[$minIndex]) {
-//             $minIndex = $j;
-//         }
-//     }
-//     if ($minIndex != $i) {
-//         $temp = $arr[$i];
-//         $arr[$i] = $arr[$minIndex];
-//         $arr[$minIndex] = $temp;
-//     }
-// }
+for ($i = 0; $i < $cntArr; $i++) {
+    $minIndex = $i;
+    for ($j = $i + 1; $j < $cntArr; $j++) {
+        if ($arr[$j] < $arr[$minIndex]) {
+            $minIndex = $j;
+        }
+    }
+    if ($minIndex != $i) {
+        $temp = $arr[$i];
+        $arr[$i] = $arr[$minIndex];
+        $arr[$minIndex] = $temp;
+    }
+}
 
-// for ($i = 0; $i < $cntArr; $i++) {
-//     echo $i . " > " . $arr[$i] . "\n";
-// }
+for ($i = 0; $i < $cntArr; $i++) {
+    echo $i . " > " . $arr[$i] . "\n";
+}
 
 
 
