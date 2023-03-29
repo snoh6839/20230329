3652cb704c7b9a3ed517b7cfa4c570b9902450b7
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-03-29 12:00:30 +0900

    error delete

--- arraysort.php
+++ arraysort.php
@@ -47,36 +47,6 @@ for ($i = 0; $i < $cntArr; $i++) {
 }
 
 
-//3
-
-$arr = array(5, 10, 7, 3, 1);
-
-for ($i = 0; $i < count($arr) - 1; $i++) {
-    $minIndex = $i;
-    $maxIndex = count($arr) - 1;
-    for ($j = $i + 1; $j < count($arr) - $i; $j++) {
-        if ($arr[$j] < $arr[$minIndex]) {
-            $minIndex = $j;
-        }
-        if ($arr[$j] > $arr[$maxIndex]) {
-            $maxIndex = $j;
-        }
-    }
-    if ($minIndex != $i) {
-        $temp = $arr[$i];
-        $arr[$i] = $arr[$minIndex];
-        $arr[$minIndex] = $temp;
-    }
-    if ($maxIndex != count($arr) - 1 - $i) {
-        $temp = $arr[count($arr) - 1 - $i];
-        $arr[count($arr) - 1 - $i] = $arr[$maxIndex];
-        $arr[$maxIndex] = $temp;
-    }
-}
-
-for ($i=0; $i < count($arr); $i++) {
-    echo $i . ">" . $arr[$i] . "\n";
-}
 
 
 ?>
