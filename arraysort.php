0d4f22b202d3284ccd1ae642b3aaf3d6bc4a1777
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-03-29 10:51:42 +0900

    Update arraysort.php

--- arraysort.php
+++ arraysort.php
@@ -3,4 +3,21 @@ $arr = array(5, 10, 7, 3, 1);
 for ($i=0; $i < count($arr) ; $i++) {
     echo $i.">".$arr[$i]."\n";
 }
+
+$arr = array(5, 10, 7, 3, 1);
+
+for ($i = 0; $i < count($arr); $i++) {
+    for ($j = 0; $j < count($arr) - $i - 1; $j++) {
+        if ($arr[$j] > $arr[$j+1]) {
+            $temp = $arr[$j];
+            $arr[$j] = $arr[$j+1];
+            $arr[$j+1] = $temp;
+        }
+    }
+}
+
+for ($i=0; $i < count($arr); $i++) {
+    echo $i . ">" . $arr[$i] . "\n";
+}
+
 ?>
